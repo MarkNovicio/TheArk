3b6c5e7ef734f1bbe0dd795f62af01a26f2e603a
Mark Novicio <marknovicio@gmail.com>
2021-11-09 00:51:07 -0500

    update contact page

--- TheArk_site_app/views.py
+++ TheArk_site_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def index(request):
     return render(request, "index.html")
 
 def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['name']
+        message_email = request.POST['email']
+        message = request.POST['message']
+
+        #send an email
+        send_mail(
+            name, 
+
+        )
     return render(request, "contact.html")
